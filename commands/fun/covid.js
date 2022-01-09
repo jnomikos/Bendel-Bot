b23eed4d8ad12326ded1c08565eb35fc6420e3a6
jnomikos <john.nomikos55@gmail.com>
2022-01-09 17:02:08 -0600

    Create covid.js

---
+++ commands/fun/covid.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'covid',
+    directory: __dirname,
+    description: '',
+    async execute(client, message, args) {
+        message.reply('<a:murderer:929246970869067787>');
+
+    }
+
+}
