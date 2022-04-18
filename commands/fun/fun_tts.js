bd95b42ed73920d3078c3f8fb96c232baa0bac35
jnomikos <john.nomikos55@gmail.com>
2022-04-18 18:01:24 -0500

    test

--- commands/fun/fun_tts.js
+++ commands/fun/fun_tts.js
@@ -86,6 +86,7 @@ module.exports = {
             })
 
             childPython.stderr.on('data', (data) => {
+                console.log("ERRPR: ", data.toString())
                 nosuccess(data);
             });
 
